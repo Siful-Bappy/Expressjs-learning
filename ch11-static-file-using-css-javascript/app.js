a52c0a896484cce63eab6845e1ede06f151623d3
Siful Bappy <bappy.ac.bd@gmail.com>
2022-11-06 00:14:00 +0900

    complete static method

--- ch11-static-file-using-css-javascript/app.js
+++ ch11-static-file-using-css-javascript/app.js
@@ -7,9 +7,23 @@ const port = 3000
 // static files
 // app.use(express.static("public"))
 // static files use by using join method
-app.use(express.static(join(process.cwd(), "public")));
+// app.use(express.static(join(process.cwd(), "public")));
 // app.use("/static", express.static(join(process.cwd(), "public")));
-console.log(join(process.cwd(), "public"))
+// console.log(join(process.cwd(), "public"))
+
+const options = {
+    dotfiles: "ignore", 
+    etag: false,
+    extensions: ["htm", "html"],
+    index: false,
+    maxAge: "1d",
+    redirect: false,
+    setHeaders: function(res, path, stat) {
+        res.set("x-timestap", Date.now())
+    }
+}
+
+app.use(express.static(join(process.cwd(), "public"), options));
 
 app.use("/", web);
 
