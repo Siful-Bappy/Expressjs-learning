e7e0f24bbff820988b5c9dfa30c3198aec8214d3
Siful Bappy <bappy.ac.bd@gmail.com>
2022-11-07 23:30:27 +0900

    add multiple doc

--- ch17-schema-and-model-in-express/app.js
+++ ch17-schema-and-model-in-express/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import connectDB from './db/connectdb.js';
-import "./models/Student.js"
+// import "./models/Student.js"
+import createDoc from './models/Student.js';
 const app = express()
 const port = 3000
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017"
 
 connectDB(DATABASE_URL)
+// createDoc();
+createDoc("Bappy", 29, 8888.00, ["Gaming", "Danching"], [{value: "This is message"}]);
 
+
+// for multiple
+// createDoc();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- ch17-schema-and-model-in-express/models/Student.js
+++ ch17-schema-and-model-in-express/models/Student.js
@@ -6,14 +6,67 @@ import mongoose from "mongoose";
 const studnetSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   age: { type: Number, required: true, min: 18, max: 50 },
-  fees: {type: mongoose.Decimal128, required: true, validate: v => v >= 5500.50},
+  fees: {
+    type: mongoose.Decimal128,
+    required: true,
+    validate: (v) => v >= 5500.5,
+  },
   hobbies: { type: Array },
   isActive: { type: Boolean },
-  comments: [{value: {type: String}, publish: {type: Date, default: Date.now }}],
+  comments: [
+    { value: { type: String }, publish: { type: Date, default: Date.now } },
+  ],
   join: { type: Date, default: Date.now },
 });
 
-console.log(studnetSchema.path("age"))
+// console.log(studnetSchema.path("age"));
 
 // compiling schema
 const studnetModel = mongoose.model("student", studnetSchema);
+
+
+
+
+// const createDoc = async () => {
+//   try {
+//     // Create new document
+//     const studnetDoc = new studnetModel({
+//       name: "Bappy",
+//       age: 24,
+//       fees: 6500,
+//       hobbies: ["Hiking", "Guiter"],
+//       comments: [{ value: "This is good mongoose" }],
+//     });
+
+//     // saving doucment
+//     const result = await studnetDoc.save();
+//     console.log(result);
+//   } catch (e) {
+//     console.log("The erro is coming from: ", e);
+//   }
+// };
+
+
+const createDoc = async (name, age, fees, hobbies, comments) => {
+    try {
+      // Create new document
+      const studnetDoc = new studnetModel({
+        name: name,
+        age: age,
+        fees: fees,
+        hobbies: hobbies,
+        comments: comments,
+      });
+  
+      // saving doucment
+      const result = await studnetDoc.save();
+
+      // insert many
+      // const result = await createDoc.insertMany([studentDoc])
+      console.log(result);
+    } catch (e) {
+      console.log("The erro is coming from: ", e);
+    }
+  };
+
+export default createDoc;
