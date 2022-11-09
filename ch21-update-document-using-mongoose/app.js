eafb8a7d0addd917d5dceef35292d6f2a701cf43
Siful Bappy <bappy.ac.bd@gmail.com>
2022-11-09 15:43:56 +0900

    update is complete

--- ch21-update-document-using-mongoose/app.js
+++ ch21-update-document-using-mongoose/app.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import connectDB from './db/connectdb.js'
-import { getAllDoc } from './models/Student.js'
+import { updateBuId } from './models/Student.js'
 const app = express()
 const port = 3000
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017"
 
 connectDB(DATABASE_URL)
 // studentDoc();
-getAllDoc();
+updateBuId("636b3906e00e72b5a8587467");
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- ch21-update-document-using-mongoose/models/Student.js
+++ ch21-update-document-using-mongoose/models/Student.js
@@ -31,13 +31,46 @@ const studentModel = mongoose.model("Student", studentSchema);
 //   } catch (e) {}
 // };
 
-const getAllDoc = async() => {
+// const getAllDoc = async() => {
+//     try {
+//         const result = await studentModel.find();
+//         console.log(result);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+
+// update by id
+// const updateBuId = async(id) => {
+//     try {
+//         // const result = await studentModel.findByIdAndUpdate(id, {name: "Ruble", age: 24})
+//         const result = await studentModel.findByIdAndUpdate(id, {name: "Hamim", age: 24}, {returnDocument: "after"})
+//         console.log(result);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+// update by one
+// const updateBuId = async(id) => {
+//     try {
+//         // const result = await studentModel.findByIdAndUpdate(id, {name: "Ruble", age: 24})
+//         // const result = await studentModel.updateOne({_id: id}, {name: "Jennie"})
+//         // will update if it find the id otherwise update by create a new one
+//         const result = await studentModel.updateOne({_id: id}, {name: "Jennie"}, {upsert: true})
+//         console.log(result);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+
+// update multiple
+const updateBuId = async(id) => {
     try {
-        const result = await studentModel.find();
+        const result = await studentModel.updateMany({_id: id}, {name: "Jennie", age: 24}, {upsert: true})
         console.log(result);
     } catch (e) {
         console.log(e);
     }
 }
 
-export {getAllDoc};
+export {updateBuId};
